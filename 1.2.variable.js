dd10c5899fd8ea0fc35a5faec9be137ea7a48926
Sampatij2023 <58723466+Sampatij2023@users.noreply.github.com>
2020-06-05 04:35:51 +0530

    Create 1.2.variable.js

---
+++ 1.2.variable.js
@@ -0,0 +1,19 @@
+let name ='Deepak'
+let score = 100
+//let is used to create variables, javascript is smart enough to know the type of variables
+so no need to specify that//
+
+in inverted commas everything is a string
+let bonus = '20'
+let totalScore = score + bonus
+console.log(name)
+console.log(totalScore) // prints 10020 i.e., concatenating the strings//
+
+// you can not redeclare a variable//
+
+//logging two valuea at the same time//
+ let firstName='Deepak'
+ let lastname ='Suthar'
+ console.log(firstname + lastName)
+ //if you want to give space between them  then see below//
+ console.log(firstname +" "+ lastName)
